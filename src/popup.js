14ecb4c338b63be294d67130ab1f5b44d66541f8
Tony Yao <tony.yao@tophatmonocle.com>
2022-05-21 22:08:29 -0400

    feat: make already bolded text even bolder

--- src/popup.js
+++ src/popup.js
@@ -18,32 +18,42 @@ changeColor.addEventListener("click", async () => {
 // The body of this function will be executed as a content script inside the current page
 function convertToReadbaleText() {
   chrome.storage.sync.get("color", async ({ color }) => {
-    
+
     // setting global styles
     var style = document.createElement("style");
-    style.textContent = "b { font-weight: bold !important; }";
+    style.textContent =
+      `bionic-reading-bold {
+        font-weight: bold !important;
+      }
+      bionic-reading-extra-bold {
+        font-weight: bold !important;
+        text-shadow:0px 0px 1px black;
+      }`
     document.head.appendChild(style);
-	
-	let tags = ["p", "font", "span"];
-	
-	tags.forEach((element) => {
-		pList = document.getElementsByTagName(element);
-		// making half of the letters in a word bold
-		for (let sentence of pList) {
-		  const sentenceText = sentence.innerText;
-		  const textArr = sentenceText.split(" ");
-		  const textArrTransformed = textArr.map((word) => {
-			const length = word.length;
-			const midPoint = Math.round(length / 2);
-			const firstHalf = word.slice(0, midPoint);
-			const secondHalf = word.slice(midPoint);
-			const htmlWord = `<b>${firstHalf}</b>${secondHalf}`;
-			return htmlWord;
-		  });
-		  console.log();
-		  sentence.innerHTML = textArrTransformed.join(" ");
-		}
-	});
+
+    let tags = ["p", "font", "span"];
+
+    tags.forEach((element) => {
+      pList = document.getElementsByTagName(element);
+      // making half of the letters in a word bold
+      for (let sentence of pList) {
+        const sentenceText = sentence.innerText;
+        const fontWeight = getComputedStyle(sentence).fontWeight || 0
+        const textArr = sentenceText.split(" ");
+        const textArrTransformed = textArr.map((word) => {
+          const length = word.length;
+          const midPoint = Math.round(length / 2);
+          const firstHalf = word.slice(0, midPoint);
+          const secondHalf = word.slice(midPoint);
+          if (fontWeight < 700) {
+            return `<bionic-reading-bold>${firstHalf}</bionic-reading-bold>${secondHalf}`;
+          } else {
+            return `<bionic-reading-extra-bold>${firstHalf}</bionic-reading-extra-bold>${secondHalf}`;
+          }
+        });
+        sentence.innerHTML = textArrTransformed.join(" ");
+      }
+    });
 
   });
 }
