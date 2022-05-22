6e5cd750d6f2c1a9c5226dc6640616074445de6f
Joey <19317273+JWW127@users.noreply.github.com>
2022-05-21 23:35:34 -0500

    added pre tag for txt converting

--- src/popup.js
+++ src/popup.js
@@ -18,32 +18,30 @@ changeColor.addEventListener("click", async () => {
 // The body of this function will be executed as a content script inside the current page
 function convertToReadbaleText() {
   chrome.storage.sync.get("color", async ({ color }) => {
-    
     // setting global styles
     var style = document.createElement("style");
     style.textContent = "b { font-weight: bold !important; }";
     document.head.appendChild(style);
-	
-	let tags = ["p", "font", "span"];
-	
-	tags.forEach((element) => {
-		pList = document.getElementsByTagName(element);
-		// making half of the letters in a word bold
-		for (let sentence of pList) {
-		  const sentenceText = sentence.innerText;
-		  const textArr = sentenceText.split(" ");
-		  const textArrTransformed = textArr.map((word) => {
-			const length = word.length;
-			const midPoint = Math.round(length / 2);
-			const firstHalf = word.slice(0, midPoint);
-			const secondHalf = word.slice(midPoint);
-			const htmlWord = `<b>${firstHalf}</b>${secondHalf}`;
-			return htmlWord;
-		  });
-		  console.log();
-		  sentence.innerHTML = textArrTransformed.join(" ");
-		}
-	});
 
+    let tags = ["p", "font", "span", "pre"];
+
+    tags.forEach((element) => {
+      pList = document.getElementsByTagName(element);
+      // making half of the letters in a word bold
+      for (let sentence of pList) {
+        const sentenceText = sentence.innerText;
+        const textArr = sentenceText.split(" ");
+        const textArrTransformed = textArr.map((word) => {
+          const length = word.length;
+          const midPoint = Math.round(length / 2);
+          const firstHalf = word.slice(0, midPoint);
+          const secondHalf = word.slice(midPoint);
+          const htmlWord = `<b>${firstHalf}</b>${secondHalf}`;
+          return htmlWord;
+        });
+        console.log();
+        sentence.innerHTML = textArrTransformed.join(" ");
+      }
+    });
   });
 }
