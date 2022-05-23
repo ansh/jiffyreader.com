0e82e43f1e8ce88e5cf95503f572c0e97e6701f4
Peter Cseik <peter@ampsuite.com>
2022-05-23 18:18:02 +0100

    Add list of blacklisted parent containers

--- src/convert.js
+++ src/convert.js
@@ -22,6 +22,19 @@ function highlightText(sentenceText) {
     .join(' ');
 }
 
+/*
+Recursively check if a node's parent is contained in a list of tags
+ */
+function parentIsBlacklisted(node, blacklist) {
+    if (node.parentElement?.tagName === undefined) {
+        return false;
+    }
+    if (blacklist.indexOf(node.parentElement.tagName) >= 0) {
+        return true;
+    }
+    return parentIsBlacklisted(node.parentElement, blacklist);
+}
+
 function main() {
   // check if we have already highlighted the text
   const boldedElements = document.getElementsByTagName('br-bold');
@@ -39,17 +52,22 @@ function main() {
   style.textContent = '.br-bold br-bold { font-weight: bold !important; display: inline; line-height: var(--br-line-height,initial); }';
   document.head.appendChild(style);
 
-  const tags = ['p', 'font', 'span', 'li'];
+  const scannedTags = ['p', 'font', 'span', 'li'];
+  // List of tags in which if any of the above are contained, will be ignored and not highlighted
+  const blacklistParentTags = ['CODE'];
 
   const parser = new DOMParser();
-  tags.forEach((tag) => {
+  scannedTags.forEach((tag) => {
     for (const element of document.getElementsByTagName(tag)) {
+      if(parentIsBlacklisted(element, blacklistParentTags)) {
+          continue;
+      }
       const n = parser.parseFromString(element.innerHTML, 'text/html');
       const textArrTransformed = Array.from(n.body.childNodes).map((node) => {
-        if (node.nodeType === Node.TEXT_NODE) {
-          return highlightText(node.nodeValue);
-        }
-        return node.outerHTML;
+          if (node.nodeType === Node.TEXT_NODE) {
+            return highlightText(node.nodeValue);
+          }
+          return node.outerHTML;
       });
       element.innerHTML = textArrTransformed.join(' ');
     }
