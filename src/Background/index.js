1b8faebd2be0ad6df3ebcf5cd3930da8edd855f0
Ernest <asieduernest@yahoo.co.uk>
2022-06-04 18:44:21 -0400

    Feat add shortcut orginally provided by @@MegaBluejay
    probably was removed accidentally during cross_env integration

--- src/Background/index.js
+++ src/Background/index.js
@@ -1,3 +1,5 @@
+import Logger from '../Logger';
+
 const runTimeHandler = typeof browser === 'undefined' ? chrome : browser;
 
 const listener = (request, sender, sendResponse) => {
@@ -28,4 +30,36 @@ const listener = (request, sender, sendResponse) => {
       break;
   }
 };
+
+const commandListener = async (command) => {
+  Logger.logInfo('commmand fired', command);
+
+  if (command === 'toggle-bionic') {
+    const [activeTab, activeTabError] = await new Promise((res, _) => {
+      chrome.tabs.query({ active: true }, ([tab]) => {
+        if (Number.isNaN(tab?.id)) throw new Error('Error: tab is undefined');
+        res([tab, chrome.rumetime?.lastError]);
+      });
+    });
+
+    if (activeTabError) throw Logger.logError(activeTabError);
+
+    const [{ data }, getReadingModeError] = await new Promise((res, _) => {
+      chrome.tabs.sendMessage(
+        activeTab.id,
+        { type: 'getReadingMode' },
+        ((response) => {
+          res([response, chrome.runtime?.lastError]);
+        }),
+      );
+    });
+
+    if (getReadingModeError) throw Logger.logError(getReadingModeError);
+
+    chrome.tabs.sendMessage(activeTab.id, { type: 'setReadingMode', data: !data });
+  }
+};
+
+chrome.commands.onCommand.addListener(commandListener);
+
 runTimeHandler.runtime.onMessage.addListener(listener);
