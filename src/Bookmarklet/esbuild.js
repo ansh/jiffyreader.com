2008643ae1aa1344b211518a6d77fe9773958875
Ernest <asieduernest@yahoo.co.uk>
2022-06-08 17:05:59 -0400

    Update bookmarklet links for all parameters
    parameters consist of saccades interval and color, and fixation strength

--- src/Bookmarklet/esbuild.js
+++ src/Bookmarklet/esbuild.js
@@ -2,22 +2,32 @@ const { sassPlugin } = require('esbuild-sass-plugin');
 const { build } = require('esbuild');
 const fs = require('fs');
 
+const defaultConfigs = {
+  bundle: true,
+  minify: true,
+  mode: 'development',
+  write: false,
+  plugins: [sassPlugin({ type: 'css-text' })],
+};
+
 const outputFile = './extension/bookmarklet.html';
 
 build({
   entryPoints: ['./src/Bookmarklet/index.js'],
-  bundle: true,
-  minify: true,
-  write: false,
-  plugins: [sassPlugin({ type: 'css-text' })],
+  ...defaultConfigs,
 })
   .then(({ outputFiles: [res] }) => {
-    const outputText = res.text.replace(/\n/g, '');
+    const outputScript = res.text.replace(/\n/g, '');
     fs.writeFileSync(
       outputFile,
-      `<a href='javascript:${outputText}'>Toggle JiffyReader</a>
+      `
+      <a href='javascript:${outputScript.replace('ACTION_TO_FIRE', 'fireReadingToggle')};'>Toggle JiffyReader</a>
+      <a href='javascript:${outputScript.replace('ACTION_TO_FIRE', 'fireFixationStrengthTransition')};'>Toggle FixationStrength</a>
+      <a href='javascript:${outputScript.replace('ACTION_TO_FIRE', 'fireSaccadesIntervalTransition')};'>Toggle SaccadesInterval</a>
+      <a href='javascript:${outputScript.replace('ACTION_TO_FIRE', 'fireSaccadesColorTransition')};'>Toggle SaccadesColor</a>
       <br>
-      <p>Drag the link above onto your bookmark bar to save it as a bookmarklet which works on any site just like the full extension</p>
+      <p>Drag any of the links above onto your bookmark bar to save it as a bookmarklet which works on any site just like the full extension</p>
+      
   `,
     );
   })
--- src/Bookmarklet/index.js
+++ src/Bookmarklet/index.js
@@ -1,11 +1,69 @@
 import documentParser from '../ContentScript/documentParser';
+import Logger from '../Logger';
 import { defaultPrefs } from '../Preferences';
 
-const { saccadesInterval, fixationStrength } = { ...defaultPrefs, fixationStrength: 3 };
+const { saccadesInterval, fixationStrength, saccadesColor } = {
+  ...defaultPrefs,
+  fixationStrength: 3,
+};
 
-document.body.setAttribute('saccades-interval', saccadesInterval);
-document.body.setAttribute('fixation-strength', fixationStrength);
+document.body.setAttribute('saccades-interval', document.body.getAttribute('saccades-interval') ?? saccadesInterval);
+document.body.setAttribute('fixation-strength', document.body.getAttribute('fixation-strength') ?? fixationStrength);
+document.body.setAttribute('saccades-color', document.body.getAttribute('saccades-color') ?? saccadesColor);
 
-document.body.classList.toggle('br-bold');
+function toggleReadingMode() {
+  Logger.logInfo('called');
+  documentParser.setReadingMode(!document.body.classList.contains('br-bold'), document);
+}
 
-documentParser.setReadingMode(document.body.classList.contains('br-bold'), document);
+const stateTransitions = {
+  'fixation-strength': [
+    [null, 1],
+    ['', 1],
+    [1, 2],
+    [2, 3],
+    [3, 1],
+  ],
+  'saccades-interval': [
+    [null, 1],
+    ['', 1],
+    [0, 1],
+    [1, 2],
+    [2, 3],
+    [3, 4],
+    [4, 0],
+  ],
+  'saccades-color': [
+    [null, 'light'],
+    ['', 'light'],
+    ['light', 'light-100'],
+    ['light-100', 'dark'],
+    ['dark', 'dark-100'],
+    ['dark-100', ''],
+  ],
+};
+
+function toggleStateEngine(stateTransitionKey) {
+  const currentActiveState = document.body.getAttribute(stateTransitionKey);
+  Logger.logInfo('stateTransitionKey', stateTransitionKey, 'currentActiveState', currentActiveState, stateTransitions[stateTransitionKey]);
+
+  const [, nextState] = stateTransitions[stateTransitionKey]
+    .find(([state]) => `${state}` === currentActiveState);
+
+  document.body.setAttribute(stateTransitionKey, nextState);
+
+  if (!document.body.classList.contains('br-bold')) toggleReadingMode();
+}
+
+const actions = {
+  fireReadingToggle: toggleReadingMode,
+  fireFixationStrengthTransition: () => toggleStateEngine('fixation-strength'),
+  fireSaccadesIntervalTransition: () => toggleStateEngine('saccades-interval'),
+  fireSaccadesColorTransition: () => toggleStateEngine('saccades-color'),
+};
+
+const actionToFire = 'ACTION_TO_FIRE';
+
+Logger.logInfo('actionToFire', actionToFire, actions);
+
+actions[actionToFire]();
