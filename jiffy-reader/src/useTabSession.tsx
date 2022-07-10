6fbd848cbfa518e3fd64cb6ad840461bfcf6470e
Ernest <asieduernest@yahoo.co.uk>
2022-07-10 19:42:26 -0400

    Update: useTabSessions

--- jiffy-reader/src/useTabSession.tsx
+++ jiffy-reader/src/useTabSession.tsx
@@ -1,26 +1,46 @@
-import { useStorage } from '@plasmohq/storage';
 import { useEffect, useState } from 'react';
+
+import { useStorage } from '@plasmohq/storage';
 
 import Logger from '../../src/Logger';
 
-function useTabSession(getOrigin: () => Promise<string>, prefs: Prefs) {
-	const [tabSession, setTabSession] = useStorage({key:'tabSession', area: "session",},(prevSession)=> prevSession);
+function useTabSession(
+	getOrigin: () => Promise<string>,
+	getTab: () => Promise<chrome.tabs.Tab>,
+	prefs: Prefs
+): [TabSession, (UpdateCallback) => Promise<void>] {
+	const [tabSessions, setTabSessionsPrivate] = useStorage<TabSession[]>(
+		{ key: 'tabSession', area: 'local' },
+		(prevSession) => prevSession ?? []
+	);
+
+	const [tabId, setTabId] = useState(null);
+
+	const removeTab = (tabId) => setTabSessionsPrivate(tabSessions.filter((_, instanceId) => instanceId !== tabId));
 
 	useEffect(() => {
+		Logger.logInfo('initializing useTabSession');
+
 		(async () => {
-			if (!prefs || !origin) return;
+			if (!prefs) return;
 
-			setTabSession({ brMode: prefs?.onPageLoad, origin: await getOrigin() });
-			Logger.logInfo('useTabsession', await getOrigin(), { prefs });
+			const newTabID = (await getTab()).id;
+			setTabId(newTabID);
+
+			let newTabSessions = tabSessions;
+			newTabSessions[newTabID] = newTabSessions[newTabID] ?? { brMode: prefs?.onPageLoad, origin: await getOrigin() };
+			setTabSessionsPrivate(newTabSessions);
+
+			Logger.logInfo('useTabsession.effect', await getOrigin(), { prefs, tabSessions, newTabID });
 		})();
-	}, []);
+	}, [prefs]);
 
-	useEffect(() => {
-		if (!tabSession) return;
-		Logger.logInfo('new tabSession', tabSession);
-	}, [tabSession]);
+	const updateTabSession = (updateCallback: UpdateCallback) => {
+		const newTabSessions = updateCallback(tabSessions);
+		return setTabSessionsPrivate(newTabSessions);
+	};
 
-	return [tabSession, setTabSession];
+	return [prefs && tabId ? tabSessions[tabId] : null, updateTabSession];
 }
 
 export default useTabSession;
