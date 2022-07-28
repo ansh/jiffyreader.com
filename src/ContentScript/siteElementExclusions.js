7da6f4c630af2172180ca2ad3a8b2a6b5314d9fc
Ernest <asieduernest@yahoo.co.uk>
2022-07-28 02:38:56 -0400

    Fix: issue#156
    Broken grammarly input and data loss

--- src/ContentScript/siteElementExclusions.js
+++ src/ContentScript/siteElementExclusions.js
@@ -6,6 +6,7 @@ const siteElementExclusions = {
   'twitter.com': ['div.DraftEditor-root'],
   'youtube.com': ['.ytd-commentbox', 'ytd-commentbox'],
   'play.google.com': ['.mat-icon-button', 'mat-icon-button', '.scrubber-container', 'header>nav>a'],
+  'app.grammarly.com': ['.editor-editorContainer'],
 };
 
 /** @returns {Excluder} */
