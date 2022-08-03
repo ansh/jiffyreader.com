80ebd7a7b12dfae80fa5517836f09a2e741cfa5d
Ernest <asieduernest@yahoo.co.uk>
2022-08-03 04:32:47 -0400

    Chore: refactoring clones

---
+++ src/Preferences/index.js
@@ -0,0 +1,277 @@
+import Logger from '../services/Logger';
+import StorageHelper from '../../../src/StorageHelper';
+// default preferences
+// and source of truth
+// for both global and local prefs
+// so new preferences should be
+// added here
+
+export const defaultPrefs = {
+  onPageLoad: false,
+  scope: 'global',
+  lineHeight: 1,
+  edgeOpacity: 70,
+  saccadesColor: '',
+  saccadesStyle: 'bold-600',
+  saccadesInterval: 0,
+  fixationStrength: 2,
+  fixationEdgeOpacity: 80,
+  MAX_FIXATION_PARTS: 4,
+  FIXATION_LOWER_BOUND: 0,
+  BR_WORD_STEM_PERCENTAGE: 0.7,
+};
+
+/**
+ * called on startup or when prefs change
+ * Populated via init config.
+ * @type {subscriptionCallback[]}  -
+ */
+const subscriberCallbacks = [];
+
+/**
+ * subscriptions to call on prefs init
+ * @type {startupCallback[]} -
+ */
+const startupSubscribers = [];
+
+/**
+ * getOrigin is a call back
+ * that should return the origin of the page.
+ * Depending on the context, whether it be
+ * popup or content script, they might not
+ * have direct access to origin, so let them define
+ * how origin should be retreived
+ * Populated via init config.
+ * @type {()=>Promise<string>} -
+ */
+let getOrigin;
+
+/**
+ * init is the entry point of the preference
+ * which returns the following:
+ * - start() to kickstart preference
+ *    retrieval and dispatch that preference
+ *    for subscribers to know
+ * /
+ * - setPrefs() to update preferences
+ *    and dispatch the updates for all
+ *    subscribers
+ * /
+ * - defaultPrefs() - is a getter that
+ *    returns the defaultPreferences
+ * @param {{
+ * subscribe?: subscriptionCallback ,
+ * onStartup?: startupCallback ,
+ * getOrigin: {()=>Promise<string>}
+ * }} config
+ * @callback subscriptionCallback - called when prefs change
+ * @param {Prefs} prefs
+ * @callback startupCallback fired on when init is called
+ * @param {Prefs} prefs
+ * @typedef {defaultPrefs} Prefs
+ */
+function init(config) {
+  // currently only init can add to
+  // subscribers but it be easy
+  // to expose another api to add
+  // to subscriber if needed
+  if (config.subscribe) {
+    subscriberCallbacks.push(config.subscribe);
+  }
+  if (config.onStartup) {
+    startupSubscribers.push(config.onStartup);
+  }
+  // necessary to get the origin
+  // which is used to 'local' AKA per-site prefs
+  getOrigin = config.getOrigin;
+
+  return {
+    start,
+    setPrefs,
+    getPrefs,
+    defaultPrefs: () => defaultPrefs,
+  };
+}
+
+const isBackgroundScript = () => {
+  if (typeof chrome?.extension?.getBackgroundPage === 'function') {
+    // https://stackoverflow.com/questions/16267668/can-js-code-in-chrome-extension-detect-that-its-executed-as-content-script
+    // detect where code is running from, is it content script, popup or background?
+    // thats important to know if all 3 context call the same functions, that shared function has
+    // to know the context so it can work properly based on the context
+
+    // we know we are on background script if getBackgroundPage === window
+    return chrome.extension.getBackgroundPage() === window;
+  }
+  return false;
+};
+
+/** @returns {Promise<Prefs[]>|Promise<Prefs>} */
+async function retrievePrefsFromStorage(/** @type {string} 'local'|'global' */ action) {
+  return new Promise((resolve, reject) => {
+    if (isBackgroundScript()) {
+      StorageHelper.retrievePrefs(action).then((response) => resolve(response?.data));
+    } else {
+      chrome.runtime.sendMessage({ message: 'retrievePrefs', action }, async (response) => {
+        resolve(response?.data);
+      });
+    }
+  });
+}
+
+function storePrefsInStorage(
+  /** @type {Prefs} */ prefs,
+  /** @type {string} 'global'|'local' */ action,
+) {
+  return new Promise((resolve, reject) => {
+    if (isBackgroundScript()) {
+      StorageHelper.storePrefs(action, prefs);
+      resolve(true);
+    } else {
+      chrome.runtime.sendMessage({ message: 'storePrefs', data: prefs, action }, async (_) => {
+        resolve(true);
+      });
+    }
+  });
+}
+
+/** @returns {Promise<Prefs[]>} */
+async function retrieveLocalPrefs() {
+  return retrievePrefsFromStorage('local');
+}
+
+/** @return {Promise<Prefs[]>} */
+async function retriveGlobalPrefs() {
+  return retrievePrefsFromStorage('global');
+}
+
+// Stores local preferences into storage
+async function storeLocalPrefs(prefs) {
+  return storePrefsInStorage(prefs, 'local');
+}
+
+async function storeGlobalPrefs(prefs) {
+  return storePrefsInStorage(prefs, 'global');
+}
+
+/**
+ * Returns the prefs for the ogirin if available else returns global prefs
+ * @returns {Promise<Prefs>}
+ */
+async function getPrefs() {
+  let result;
+
+  try {
+    result = (await retrieveLocalPrefs())[await getOrigin()];
+  } catch (error) {
+    result = await retriveGlobalPrefs();
+  }
+  return result;
+}
+
+/**
+ * setPrefs updates the preferences in storage
+ * and dispatch this update to all subscribers
+ * prefs - has the shape of defaultPrefs
+ * or a call back that returns that shape
+ * but you only need to pass the actual fields
+ * you want to update.
+ *
+ */
+async function setPrefs(/** @type {(prefs:Prefs)=>{}|Prefs} */ prefs) {
+  // grab the current prefs
+  const localPrefs = await retrieveLocalPrefs();
+  let globalPrefs = await retriveGlobalPrefs();
+  const origin = await getOrigin();
+
+  // if local
+  const currentPrefs = {
+    ...(localPrefs[origin]?.scope === 'local' ? localPrefs[origin] : globalPrefs),
+  };
+
+  // if prefs is a function, pass in the current
+  // prefs based on the scope
+  // and newPrefs will be the return val of the function
+  // otherwise the newPrefs will just be the prefs
+
+  const newPrefs = {
+    ...currentPrefs,
+    ...(typeof prefs === 'function' ? prefs(currentPrefs) : prefs),
+  };
+
+  if (newPrefs.scope === 'global') {
+    // write scope to global prefs
+    globalPrefs = {
+      ...globalPrefs,
+      // write globalPrefs only if localPrefs[origin] has been removed prior
+      ...(!localPrefs[origin] ? newPrefs : null),
+      scope: newPrefs.scope,
+    };
+
+    if (localPrefs[origin]) delete localPrefs[origin];
+  } else {
+    localPrefs[origin] = { ...localPrefs[origin], ...newPrefs };
+  }
+
+  dispatchPrefsUpdate(localPrefs[origin] ?? globalPrefs);
+
+  storeLocalPrefs(localPrefs);
+  storeGlobalPrefs(globalPrefs);
+}
+
+/**
+ * start - kickstarts the retrieval of the current preference
+ * and dispatch that prefs to subscribers
+ * for example usage, see Preference.init on
+ * Popup/index.js and ContentScript/index.js
+ */
+async function start() {
+  debugger;
+  Logger.logInfo('start fired prefs');
+  let localPrefs = await retrieveLocalPrefs();
+  let globalPrefs = await retriveGlobalPrefs();
+  const origin = await getOrigin();
+
+  if (localPrefs == null) {
+    localPrefs = {};
+  }
+
+  // just in case their missing a default prefs
+  // be sure to pepper the default prefs
+  // localPrefs[origin] = { ...defaultPrefs, ...localPrefs[origin] };
+  globalPrefs = { ...defaultPrefs, ...globalPrefs };
+  Logger.logInfo('set globalPrefs', { globalPrefs, origin });
+
+  storeLocalPrefs(localPrefs);
+  storeGlobalPrefs(globalPrefs);
+  Logger.logInfo('start.storePrefs', { globalPrefs, localPrefs });
+
+  if (localPrefs[origin]?.scope === 'local') {
+    dispatchPrefsUpdate(localPrefs[origin]);
+    dispatchPrefsUpdate(localPrefs[origin], startupSubscribers);
+  } else {
+    dispatchPrefsUpdate(globalPrefs);
+    dispatchPrefsUpdate(globalPrefs, startupSubscribers);
+  }
+}
+
+/**
+ * calls all subscriber call back
+ * and pass them the preference via prefs
+ * params
+ */
+function dispatchPrefsUpdate(/** @type Prefs */ prefs, /** @type {Function[]} */ cbs) {
+  if (Array.isArray(cbs)) {
+    cbs.forEach((cb) => {
+      cb(prefs);
+    });
+  } else {
+    subscriberCallbacks.forEach((cb) => {
+      cb(prefs);
+    });
+  }
+}
+
+export default {
+  init,
+};
---
+++ src/services/TabHelper.js
@@ -0,0 +1,61 @@
+import Logger from './Logger';
+
+const isBackgroundScript = () => {
+  if (typeof chrome?.extension?.getBackgroundPage === 'function') {
+    // https://stackoverflow.com/questions/16267668/can-js-code-in-chrome-extension-detect-that-its-executed-as-content-script
+    // detect where code is running from, is it content script, popup or background?
+    // thats important to know if all 3 context call the same functions, that shared function has
+    // to know the context so it can work properly based on the context
+
+    // we know we are on background script if getBackgroundPage === window
+    return chrome.extension.getBackgroundPage() === window;
+  }
+  return false;
+};
+
+/** @returns {Promise<chrome.tabs.Tab>} */
+const getActiveTab = (isBgScript) => new Promise((res, rej) => {
+  try {
+    if (isBgScript) {
+      chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => {
+        Logger.logInfo(activeTab);
+        res(activeTab);
+      });
+    } else {
+      chrome.runtime.sendMessage(
+        { message: 'getActiveTab' },
+        ({ /** @type {chrome.tabs.Tab} */ data }) => {
+          res(data);
+        },
+      );
+    }
+  } catch (error) {
+    rej(error);
+    Logger.logError(error);
+  }
+});
+
+/**
+ * @params {chrome.tabs.Tab} [tab = getActiveTab()]
+ * @returns {Promise<string>} -
+ * the origin of the tab provided or the origin of the active tab if tab is null
+ */
+const getTabOrigin = async (/** @type {chrome.tabs.Tab} */ tab) => new Promise((res, rej) => {
+  try {
+    chrome.tabs.sendMessage(
+      tab.id,
+      { type: 'getOrigin' },
+      (/** @type {{data: string}} */ { data }) => {
+        const originError = chrome.runtime?.lastError;
+        if (originError) throw originError;
+
+        res(data);
+      },
+    );
+  } catch (err) {
+    Logger.LogLastError(err);
+    rej(err);
+  }
+});
+
+export default { getActiveTab, getTabOrigin, isBackgroundScript };
