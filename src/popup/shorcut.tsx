e7e5902a412e7dfadab4654165314cb9b0f88417
linuxdev <asieduernest@yahoo.co.uk>
2023-07-07 17:07:12 -0400

    Refactor: runtimehandler
    Feat: shortcut and shortcut guide component

---
+++ src/popup/shorcut.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+
+import Logger from '~services/Logger';
+import runTimeHandler from '~services/runTimeHandler';
+
+export function useShortcut() {
+	const [shortcut, setShortcut] = useState(undefined);
+
+	const getShortcut = () => {
+		runTimeHandler.runtime
+			.sendMessage({ message: 'getShortcut' })
+			.then((shortcutResponse) => setShortcut(shortcutResponse))
+			.catch(Logger.logError);
+	};
+
+	useEffect(getShortcut, []);
+
+	return shortcut;
+}
+export function ShortcutGuide() {
+	const shortcutGuide = <a href="https://jiffyreader.com/api/destinations/readme_shortcut" target='_blank'>How to setup custom shortcut</a>;
+	const shortcut = useShortcut();
+	return shortcut ? <></> : shortcutGuide;
+}
+
+export default function Shortcut({ scope = 'new' }: { scope: 'new' | 'old' }) {
+	const shortcut = useShortcut();
+
+	if (!shortcut) {
+		return <></>;
+	}
+
+	if (scope === 'old') {
+		return (
+			<span>
+				{chrome.i18n.getMessage('defaultShortcutLabelText')}:{' '}
+				{/* {chrome.i18n.getMessage(/firefox/i.test(process.env.TARGET) ? 'defaultShortcutValueTextFirefox' : 'defaultShortcutValueTextChrome')} */}
+				{shortcut}
+			</span>
+		);
+	}
+
+	if (scope === 'new') {
+		return (
+			<span>
+				{chrome.i18n.getMessage('defaultShortcutLabelText')}:
+				{/* {chrome.i18n.getMessage(/firefox/i.test(process.env.TARGET) ? 'defaultShortcutValueTextFirefox' : 'defaultShortcutValueTextChrome')} */}
+				{shortcut}
+			</span>
+		);
+	}
+
+	return <div></div>;
+}
