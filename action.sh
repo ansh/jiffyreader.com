7a43ee6b134fc2616e670c5f79ede41b94c516e3
linuxdev <asieduernest@yahoo.co.uk>
2023-09-02 06:17:29 -0400

    Update(dev): simply action.sh and simplify its call to just a single line with default parameters to be called for building all browser targets except safari

--- action.sh
+++ action.sh
@@ -1,8 +1,86 @@
-ext_version_name=1-beta
-echo $ext_version_name
-ext_version_id=$(echo $ext_version_name | tr -d [:alpha:] | tr -d [=-=])
-ext_tag=$(echo $ext_version_name | tr -d [:digit:] | tr -d [=-=] | tr -d [=.=])
-echo "build jiffy-reader-development"
-printf "VERSION=$ext_version_id \nNAME=\"Jiffy Reader $ext_tag\" \nTARGET=chrome \nDEBUG=FALSE" > .env.production
-pnpm gh:build:development
-zip build/jiffyReader-chrome-$ext_tag -j build/chrome-mv3-prod/*
+#!/bin/bash
+
+appRoot="$(pwd)"
+# take tag name as args
+
+goToAppRoot(){
+    cd "$appRoot"
+}
+
+
+printMsg(){
+    msg=$1
+    echo ">>> $msg"
+}
+
+getVersion(){
+    # input: "version-scope"
+    tag=$1
+    version=${tag%%-development}
+    echo $version
+}
+
+getScope(){
+    tag=$1
+    # 10-development
+    suffix="$2"
+    scope_raw="${tag#${suffix}}"
+    scope="${scope_raw//-/}"
+    echo $scope
+}
+
+buildEnv(){
+    version="$(getVersion $1)"
+    scope=$2
+    fullTag=$3
+    echo "version: ${version}"
+    echo "scope: $(getScope $fullTag $version )"
+    echo "name: $fullTag"
+
+    printf "VERSION=$fullTag\nNAME=Jiffy Reader$scope\nTARGET=chrome\nDEBUG=FALSE\nSHORTCUT=Alt+B\n" > .env.production
+}
+
+
+zipPacks(){
+    scope=$1
+    cd build/chrome-mv3-prod/ && zip -r "../jiffyReader-chrome$scope" ./*
+    goToAppRoot
+    cd build/firefox-mv2-prod/ && zip -r "../jiffyReader-firefox$scope.xpi" ./*
+    goToAppRoot
+    printMsg "zipPacks complete $(ls build/ -l)"
+}
+
+
+
+buildDevelopment(){
+    printMsg "buildDevelopment:started"
+    fullname="$1-development"
+    buildEnv $1 " Dev" $fullname
+    pnpm gh:build:development && pnpm gh:build:bookmarklet && pnpm gh:build:firefox
+    printMsg "buildDevelopment:complete"
+    zipPacks "-development"
+}
+
+buildRelease(){
+    fullname="$1"
+    buildEnv $1 " " $fullname
+    pnpm gh:build:chrome && pnpm gh:build:firefox && pnpm gh:build:bookmarklet
+    zipPacks ""
+}
+
+handle(){
+    tag=$1
+    buildDevelopment $tag 
+    buildRelease $tag
+}
+
+cleanBuildDirectory(){
+    rm -rf build/
+}
+
+
+tag=$1
+cleanBuildDirectory
+
+# tags= value in bump.txt if not set
+handle "${tag:=$(cat bump.txt)}"
