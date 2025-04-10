d7916d4e8fc7061c08e60a1522fda4feef78a774
linuxdev <asieduernest@yahoo.co.uk>
2025-04-10 09:09:35 -0400

    fix: remove extraneous permissions related to mellowtel

--- src/contents/content_script.ts
+++
@@ -1,9 +0,0 @@
-import type { PlasmoContentScript } from 'plasmo';
-
-export const config: PlasmoContentScript = {
-	matches: ['<all_urls>'],
-	all_frames: true,
-	run_at: 'document_start',
-};
-
-export {};
